4863d895d72c18aacc00cefd7bac36a4011ed20b
ahsanch007 <cahsan562@gmail.com>
2024-11-07 13:45:40 +0500

    done page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customPink: 'rgb(230, 199, 235)', // Add a name for your custom color
+        },
+      },
+    },
+  };
+  
